a7359cea3c7659adee351f193c898b282122d5a0
Andie Pérez <andieperezchavez@gmail.com>
2026-05-05 23:57:20 -0400

    tarea1 terminada

--- index.js
+++ index.js
@@ -24,6 +24,78 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', function(req, res) {
+  let dateInput = req.params.date;
+
+  if(!dateInput){
+    var date = new Date();
+    var unix = date.getTime();
+    res.json({
+      unix: parseInt(unix),
+      utc: date.toUTCString()
+    });
+  }else if (isValidDateFormat(dateInput)){
+    var date = new Date(dateInput);
+    var unix = date.getTime();
+    res.json({
+      unix: parseInt(unix),
+      utc: date.toUTCString()
+    });
+  }else if (isValidUnixFormat(dateInput)){
+    var date = new Date(parseInt(dateInput));
+    var unix = dateInput;
+    res.json({
+      unix: parseInt(unix),
+      utc: date.toUTCString()
+    });
+  }else{
+    res.json({ error : "Invalid Date" });
+  }
+});
+
+function isValidDateFormat(dateStr){
+  var value = dateStr.split("-");
+  if (value.length != 3){
+    //console.log("A");
+    return false;
+  }
+  var year = value[0];
+  var month = value[1];
+  var day = value[2];
+
+  if (/^\d+$/.test(year) == false){
+    //console.log("B");
+    return false;
+  }
+  if (/^\d+$/.test(month) == false){
+    //console.log("C");
+    return false;
+  }
+  if (/^\d+$/.test(day) == false){
+    //console.log("D");
+    return false;
+  }
+
+  const fecha = new Date(year, month - 1, day);
+  //console.log("final test");
+  //console.log(fecha);
+  return (
+    fecha.getFullYear() === parseInt(year) &&
+    fecha.getMonth() === parseInt(month) - 1 &&
+    fecha.getDate() === parseInt(day)
+  );
+}
+
+function isValidUnixFormat(dateStr){
+  if (/^\d+$/.test(dateStr) == false){
+    return false;
+  }
+  const date = new Date(Number(dateStr));
+  if (date.toString() === "Invalid Date"){
+    return false;
+  }
+  return true;
+}
 
 
 // Listen on port set in environment variable or default to 3000
